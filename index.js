0d54c74b66de06260683b8882f3f1c10b308de02
BigyanPradhan <bp47041721@student.ku.edu.np>
2023-11-26 21:45:52 +0545

    Question 2 solved. question 1 code done answer to be checked

--- index.js
+++ index.js
@@ -16,11 +16,135 @@ app.post('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/solve', (req, res) => {
+app.post('/q1', (req, res) => {
     // Get user inputs
-    const equationInput = req.body.equation;
-    const initialGuess = parseFloat(req.body.initialGuess);
-    const tol = req.body.error;
+    const equationInput = "x^2-sin(x)";
+    const initialGuess1 = parseFloat(0.5);
+    const initialGuess2 = parseFloat(1.0);
+    const tol = 4;
+    let a;
+    let b;
+    
+    // Define tolerance and maximum iterations
+    const tolerance = 1/Math.pow(10,tol);
+    const maxIterations = 100;
+
+    // Initialize iteration counter and x-value
+    let iteration = 0;
+
+    // Define the equation as a function
+    const equationFunction = math.parse(equationInput).compile();
+
+    const fxVal1 = equationFunction.evaluate({ x: initialGuess1 });
+    
+    //Set a and b according to their odd-even state
+    if(fxVal1 < 0)
+    {
+        a = initialGuess1;
+        b = initialGuess2;
+    }
+    else
+    {
+        b = initialGuess1;
+        a = initialGuess2;
+    }
+
+    let resultsHTML= `<table><tr><th>Iteration</th><th>a</th><th>b</th><th>s</th><th>Remarks on f(s)</th></tr>`;
+
+    while (iteration < maxIterations) {
+        try {
+            // Calculate the function value at a and b
+            const fxa = equationFunction.evaluate({ x: a });
+            const fxb = equationFunction.evaluate({ x: b });
+
+            //Formula for bisection method
+            s=(a+b)/2;
+
+            //Calculate the function value at s
+            const fxs = equationFunction.evaluate({ x: s });
+            
+            //Initiate remarks and it's conditions
+            let Remarks;
+            if(fxs<0)
+            {
+                Remarks= "f(s)<0";
+            }
+            else
+            {
+                Remarks= "f(s)>0";
+            }
+
+            // Check for convergence
+            if (Math.abs(fxs) < tolerance) {
+                break;
+            }
+
+            // Append current iteration details to results HTML
+            resultsHTML += `<tr><td>${iteration}</td><td>${a.toFixed(6)}</td><td>${b.toFixed(6)}</td><td>${s.toFixed(6)}</td>
+            <td>${Remarks}</td></tr>`;
+
+            //Update the value with s
+            if(fxs<0)
+            {
+                a=s;
+            }
+            else
+            {
+                b=s;
+            }
+
+            iteration++;
+        } catch (error) {
+            console.error(error);
+            resultsHTML += `<tr><td colspan="3">Error occurred during evaluation.</td></tr>`;
+            break;
+        }
+    }
+
+    if (iteration === maxIterations) {
+        resultsHTML += `<tr><td colspan="3">Maximum iterations reached. The method did not converge.</td></tr>`;
+    }
+
+    resultsHTML += '</table>';
+
+    // Display the results
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Newton-Raphson Solver</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    </head>
+    <body background="resources/mcsc.png">
+        <b>
+        <h1>Newton-Raphson </h1>
+        <h1>Solver</h1>
+                <form id="solver-form" action="/" method="post">
+                <div class="prev">
+                <p> Equation = ${equationInput} </p>
+                <p>Initial Guess:   a = ${a} || b = ${b}</p>
+                <p>The Solution correct to ${tol} digits is found by:</p>
+                </div>
+                <div class="text">
+                <p>${resultsHTML}</p>
+                <p>Converged to solution:${s.toFixed(tol)}</p>
+
+                <center>
+                    <input type="submit" class="btn" value="Back">
+                </center>
+                </div>
+                </form>
+        </b>
+    </body>
+    </html>
+`);
+});
+
+app.post('/q2', (req, res) => {
+    // Get user inputs
+    const equationInput = "e^x-4*x";
+    const initialGuess = parseFloat(1);
+    const tol = 6;
     // Define tolerance and maximum iterations
    
     const tolerance = 1/Math.pow(10,tol);
@@ -30,7 +154,7 @@ app.post('/solve', (req, res) => {
     let iteration = 0;
     let xVal = initialGuess;
 
-    let resultsHTML= `<table><tr><th>Iteration</th><th>x</th><th>f(x)</th><th>f'(x)</th></tr>`;
+    let resultsHTML= `<table><tr><th>Iteration</th><th>x</th><th>f(x)</th><th>f'(x)</th><th>x'n+1'</th></tr>`;
 
     // Define the equation as a function
     const equationFunction = math.parse(equationInput).compile();
@@ -45,13 +169,13 @@ app.post('/solve', (req, res) => {
             if (Math.abs(fxVal) < tolerance) {
                 break;
             }
-
+            let calc = xVal - fxVal / dfxVal;
+            // Append current iteration details to results HTML
+            resultsHTML += `<tr><td>${iteration}</td><td>${xVal.toFixed(6)}</td><td>${fxVal.toFixed(6)}</td>
+            <td>${dfxVal.toFixed(6)}</td><td>${calc.toFixed(6)}</td></tr>`;
+            
             // Update x using the Newton-Raphson formula
             xVal = xVal - fxVal / dfxVal;
-
-            // Append current iteration details to results HTML
-            resultsHTML += `<tr><td>${iteration}</td><td>${xVal.toFixed(6)}</td><td>${fxVal.toFixed(7)}</td>
-            <td>${dfxVal.toFixed(6)}</td></tr>`;
 
             iteration++;
         } catch (error) {
